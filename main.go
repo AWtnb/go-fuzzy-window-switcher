972da63bf13f4b1bd942087e9b5b36bc1926e7f1
awtnb <arikaebanataw@gmail.com>
2025-09-08 22:10:52 +0900

    fix

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ import (
 	fzf "github.com/junegunn/fzf/src"
 )
 
+// https://gist.github.com/EliCDavis/5374fa4947897b16a81f6550d142ab28
+// https://aonasuzutsuki.hatenablog.jp/entry/2019/08/28/112700#Win32API%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89%E3%81%AE%E5%AE%A3%E8%A8%80
+// https://gist.github.com/junegunn/193990b65be48a38aac6ac49d5669170
+
 var (
 	user32                    = syscall.MustLoadDLL("user32.dll")
 	procEnumWindows           = user32.MustFindProc("EnumWindows")
