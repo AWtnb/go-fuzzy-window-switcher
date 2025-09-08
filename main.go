8b77e4694370cd28468b8bf87414a5e461700df5
awtnb <arikaebanataw@gmail.com>
2025-09-08 21:57:47 +0900

    use fzf to filter

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
+	"slices"
+	"sync"
 	"syscall"
 	"unsafe"
+
+	fzf "github.com/junegunn/fzf/src"
 )
 
 var (
@@ -95,6 +100,18 @@ func isVisibleWindow(hwnd syscall.Handle) bool {
 	return ret != 0
 }
 
+func isSkippable(name string) bool {
+	ss := []string{
+		"fzf.exe",
+		"explorer.exe",
+		"MouseGestureL.exe",
+		"TextInputHost.exe",
+		"SystemSettings.exe",
+		"ApplicationFrameHost.exe",
+	}
+	return slices.Contains(ss, name)
+}
+
 func ForceForegroundWindow(hwnd syscall.Handle) bool {
 	var targetPID uint32
 	targetTID, _, _ := procGetWindowThreadProcID.Call(
@@ -125,33 +142,81 @@ func ForceForegroundWindow(hwnd syscall.Handle) bool {
 	return ret != 0
 }
 
-func FindWindow() {
-	cb := syscall.NewCallback(func(h syscall.Handle, p uintptr) uintptr {
-		if !isVisibleWindow(h) {
+func run() int {
+
+	hwndMap := make(map[string]syscall.Handle)
+
+	inputChan := make(chan string)
+	go func() {
+		cb := syscall.NewCallback(func(h syscall.Handle, p uintptr) uintptr {
+			if !isVisibleWindow(h) {
+				return 1
+			}
+
+			text, _ := getWindowText(h)
+			if text == "" {
+				return 1
+			}
+
+			pid, err := getProcessID(h)
+			if err != nil {
+				return 1
+			}
+
+			name, err := getProcessName(pid)
+			if err != nil || name == "" {
+				return 1
+			}
+			if isSkippable(name) {
+				return 1
+			}
+
+			s := fmt.Sprintf("%s[%s]", text, name)
+			inputChan <- s
+			hwndMap[s] = h
 			return 1
-		}
+		})
+		enumWindows(cb, 0)
+		close(inputChan)
+	}()
 
-		title, _ := getWindowText(h)
-		if title == "" {
-			return 1
+	var wg sync.WaitGroup
+	outputChan := make(chan string)
+	wg.Go(func() {
+		for s := range outputChan {
+			h, ok := hwndMap[s]
+			if ok {
+				if !ForceForegroundWindow(h) {
+					fmt.Printf("Failed to activate window (%d)\n", h)
+				}
+				return
+			}
 		}
-
-		pid, err := getProcessID(h)
-		if err != nil {
-			return 1
-		}
-
-		name, err := getProcessName(pid)
-		if err != nil {
-			name = "(unknown)"
-		}
-
-		fmt.Printf("PID: %5d | Visible: %-3t | Process: %-20s | Title: %s\n", pid, isVisibleWindow(h), name, title)
-		return 1
 	})
-	enumWindows(cb, 0)
+
+	options, err := fzf.ParseOptions(
+		true,
+		[]string{"--margin=1", "--no-mouse"},
+	)
+	if err != nil {
+		fmt.Println(err.Error())
+		return 1
+	}
+
+	options.Input = inputChan
+	options.Output = outputChan
+
+	code, err := fzf.Run(options)
+	close(outputChan)
+	wg.Wait()
+	if err != nil {
+		fmt.Println(err.Error())
+		return 1
+	}
+
+	return code
 }
 
 func main() {
-	FindWindow()
+	os.Exit(run())
 }
